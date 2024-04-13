9aaa49048b8ba06e1425b5022717d32c286d5e75
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2024-04-13 23:33:46 +0500

    array using arrayObject

---
+++ Arrays/arrayObject.ts
@@ -0,0 +1,5 @@
+// Initializing an Array by using the Array object. 
+let array:string[] = new Array("GEEKSFORGEEKS", "2200", "Java", "Abhishek"); 
+for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
