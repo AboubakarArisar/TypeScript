65a82c86e3b7065d9f9a691c48724d924f10754d
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2024-04-13 23:27:53 +0500

    printing the array in typescript

---
+++ Arrays/print.ts
@@ -0,0 +1,11 @@
+let fruit: string[] = ['Apple', 'Orange', 'Banana']; 
+
+for(var index in fruits) 
+{ 
+	console.log(fruit[index]); // output: Apple Orange Banana 
+} 
+
+for(var i = 0; i < fruit.length; i++) 
+{ 
+	console.log(fruit[i]); // output: Apple Orange Banana 
+}
